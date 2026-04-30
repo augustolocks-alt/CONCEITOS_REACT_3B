54030c11a4f45902a0abafe898fe283008e00c77
AUGUSTO BÚRIGO LOCKS <augusto_locks@estudante.sesisenai.org.br>
2026-04-30 11:40:28 -0300

    src

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,24 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
- const renderiarTela = () => {
-  //navegacão
- }
+const [tela,setTela1] = useState('login') 
 
-  return (
-    <>
-      <section id="center">
-        
-      </section>
+ const renderiarTela = () => {
+  if (tela === 'login') {
+  return 'login'
+  } else if (tela ==='cadastro') {
+  return 'cadastro'
+  }
+}
+const trocarTela = (pagina)=>{
+tela = pagina
+}
+return (
+  <>
+        <h1>dead cell</h1>
+      <button onClick={trocarTela('login')}>login</button>
+      <button onClick={trpacarTela('cadastro')}>cadastro</button>
+      {renderizarTela()}
     </>
   )
 }
